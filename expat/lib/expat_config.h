5395b9d58291beb226fb808860d2b2a8fadaf7e1
Boris Hristov <bhristov@esri.com>
2022-02-07 16:26:07 -0800

    Re-generate expat_config.h on Linux/Mac OS/Windows

--- expat/lib/expat_config.h
+++ expat/lib/expat_config.h
@@ -1,6 +1,6 @@
 /* This file was generated using libexpat's cmake implementation on all three OS
 types. This was done by running cmake with -G "Unix Makefiles" on Linux and
-macOS and -G "Visual Studio 2019" on Windows. The generated expat_config.h file
+macOS and -G "Visual Studio 16 2019" on Windows. The generated expat_config.h file
 in was then copied into their respective RTC ifdef blocks. Global settings are
 also defined in this file that are not part of the generated file options but
 still needed for all platforms.
@@ -13,7 +13,7 @@ still needed for all platforms.
 /* expat_config.h.cmake.  Based upon generated expat_config.h.in.  */
 
 /* 1234 = LIL_ENDIAN, 4321 = BIGENDIAN */
-#  define BYTEORDER 1234
+#define BYTEORDER 1234
 
 /* Define to 1 if you have the `arc4random' function. */
 /* #undef HAVE_ARC4RANDOM */
@@ -22,76 +22,76 @@ still needed for all platforms.
 /* #undef HAVE_ARC4RANDOM_BUF */
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#  define HAVE_DLFCN_H
+#define HAVE_DLFCN_H
 
 /* Define to 1 if you have the <fcntl.h> header file. */
-#  define HAVE_FCNTL_H
+#define HAVE_FCNTL_H
 
 /* Define to 1 if you have the `getpagesize' function. */
-#  define HAVE_GETPAGESIZE
+#define HAVE_GETPAGESIZE
 
 /* Define to 1 if you have the `getrandom' function. */
-/* #undef HAVE_GETRANDOM */
+#define HAVE_GETRANDOM
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#  define HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H
 
 /* Define to 1 if you have the `bsd' library (-lbsd). */
 /* #undef HAVE_LIBBSD */
 
 /* Define to 1 if you have the <memory.h> header file. */
-#  define HAVE_MEMORY_H
+#define HAVE_MEMORY_H
 
 /* Define to 1 if you have a working `mmap' system call. */
-#  define HAVE_MMAP
+#define HAVE_MMAP
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#  define HAVE_STDINT_H
+#define HAVE_STDINT_H
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#  define HAVE_STDLIB_H
+#define HAVE_STDLIB_H
 
 /* Define to 1 if you have the <strings.h> header file. */
-#  define HAVE_STRINGS_H
+#define HAVE_STRINGS_H
 
 /* Define to 1 if you have the <string.h> header file. */
-#  define HAVE_STRING_H
+#define HAVE_STRING_H
 
 /* Define to 1 if you have `syscall' and `SYS_getrandom'. */
-#  define HAVE_SYSCALL_GETRANDOM
+#define HAVE_SYSCALL_GETRANDOM
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#  define HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#  define HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#  define HAVE_UNISTD_H
+#define HAVE_UNISTD_H
 
 /* Name of package */
-#  define PACKAGE "expat"
+#define PACKAGE "expat"
 
 /* Define to the address where bug reports for this package should be sent. */
-#  define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
+#define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
 
 /* Define to the full name of this package. */
-#  define PACKAGE_NAME "expat"
+#define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#  define PACKAGE_STRING "expat 2.4.1"
+#define PACKAGE_STRING "expat 2.4.4"
 
 /* Define to the one symbol short name of this package. */
-#  define PACKAGE_TARNAME "expat"
+#define PACKAGE_TARNAME "expat"
 
 /* Define to the home page for this package. */
-#  define PACKAGE_URL ""
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#  define PACKAGE_VERSION "2.4.1"
+#define PACKAGE_VERSION "2.4.4"
 
 /* Define to 1 if you have the ANSI C header files. */
-#  define STDC_HEADERS
+#define STDC_HEADERS
 
 /* whether byteorder is bigendian */
 /* #undef WORDS_BIGENDIAN */
@@ -102,23 +102,23 @@ still needed for all platforms.
 
 /* Define to specify how much context to retain around the current parse
    point. */
-#  define XML_CONTEXT_BYTES 1024
+#define XML_CONTEXT_BYTES 1024
 
-#  if ! defined(_WIN32)
+#if ! defined(_WIN32)
 /* Define to include code reading entropy from `/dev/urandom'. */
-#    define XML_DEV_URANDOM
-#  endif
+  #define XML_DEV_URANDOM
+#endif
 
 /* Define to make parameter entity parsing functionality available. */
-#  define XML_DTD
+#define XML_DTD
 
 /* Define to make XML Namespaces functionality available. */
-#  define XML_NS
+#define XML_NS
 
 /* Define to __FUNCTION__ or "" if `__func__' does not conform to ANSI C. */
-#  ifdef _MSC_VER
-#    define __func__ __FUNCTION__
-#  endif
+#ifdef _MSC_VER
+#  define __func__ __FUNCTION__
+#endif
 
 /* Define to `long' if <sys/types.h> does not define. */
 /* #undef off_t */
@@ -130,85 +130,85 @@ still needed for all platforms.
 /* expat_config.h.cmake.  Based upon generated expat_config.h.in.  */
 
 /* 1234 = LIL_ENDIAN, 4321 = BIGENDIAN */
-#  define BYTEORDER 1234
+#define BYTEORDER 1234
 
 /* Define to 1 if you have the `arc4random' function. */
 /* #undef HAVE_ARC4RANDOM */
 
 /* Define to 1 if you have the `arc4random_buf' function. */
-#  define HAVE_ARC4RANDOM_BUF
+#define HAVE_ARC4RANDOM_BUF
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#  define HAVE_DLFCN_H
+#define HAVE_DLFCN_H
 
 /* Define to 1 if you have the <fcntl.h> header file. */
-#  define HAVE_FCNTL_H
+#define HAVE_FCNTL_H
 
 /* Define to 1 if you have the `getpagesize' function. */
-#  define HAVE_GETPAGESIZE
+#define HAVE_GETPAGESIZE
 
 /* Define to 1 if you have the `getrandom' function. */
 /* #undef HAVE_GETRANDOM */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#  define HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H
 
 /* Define to 1 if you have the `bsd' library (-lbsd). */
 /* #undef HAVE_LIBBSD */
 
 /* Define to 1 if you have the <memory.h> header file. */
-#  define HAVE_MEMORY_H
+#define HAVE_MEMORY_H
 
 /* Define to 1 if you have a working `mmap' system call. */
-#  define HAVE_MMAP
+#define HAVE_MMAP
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#  define HAVE_STDINT_H
+#define HAVE_STDINT_H
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#  define HAVE_STDLIB_H
+#define HAVE_STDLIB_H
 
 /* Define to 1 if you have the <strings.h> header file. */
-#  define HAVE_STRINGS_H
+#define HAVE_STRINGS_H
 
 /* Define to 1 if you have the <string.h> header file. */
-#  define HAVE_STRING_H
+#define HAVE_STRING_H
 
 /* Define to 1 if you have `syscall' and `SYS_getrandom'. */
 /* #undef HAVE_SYSCALL_GETRANDOM */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#  define HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#  define HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#  define HAVE_UNISTD_H
+#define HAVE_UNISTD_H
 
 /* Name of package */
-#  define PACKAGE "expat"
+#define PACKAGE "expat"
 
 /* Define to the address where bug reports for this package should be sent. */
-#  define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
+#define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
 
 /* Define to the full name of this package. */
-#  define PACKAGE_NAME "expat"
+#define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#  define PACKAGE_STRING "expat 2.4.1"
+#define PACKAGE_STRING "expat 2.4.4"
 
 /* Define to the one symbol short name of this package. */
-#  define PACKAGE_TARNAME "expat"
+#define PACKAGE_TARNAME "expat"
 
 /* Define to the home page for this package. */
-#  define PACKAGE_URL ""
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#  define PACKAGE_VERSION "2.4.1"
+#define PACKAGE_VERSION "2.4.4"
 
 /* Define to 1 if you have the ANSI C header files. */
-#  define STDC_HEADERS
+#define STDC_HEADERS
 
 /* whether byteorder is bigendian */
 /* #undef WORDS_BIGENDIAN */
@@ -219,23 +219,23 @@ still needed for all platforms.
 
 /* Define to specify how much context to retain around the current parse
    point. */
-#  define XML_CONTEXT_BYTES 1024
+#define XML_CONTEXT_BYTES 1024
 
-#  if ! defined(_WIN32)
+#if ! defined(_WIN32)
 /* Define to include code reading entropy from `/dev/urandom'. */
-#    define XML_DEV_URANDOM
-#  endif
+  #define XML_DEV_URANDOM
+#endif
 
 /* Define to make parameter entity parsing functionality available. */
-#  define XML_DTD
+#define XML_DTD
 
 /* Define to make XML Namespaces functionality available. */
-#  define XML_NS
+#define XML_NS
 
 /* Define to __FUNCTION__ or "" if `__func__' does not conform to ANSI C. */
-#  ifdef _MSC_VER
-#    define __func__ __FUNCTION__
-#  endif
+#ifdef _MSC_VER
+#  define __func__ __FUNCTION__
+#endif
 
 /* Define to `long' if <sys/types.h> does not define. */
 /* #undef off_t */
@@ -247,7 +247,7 @@ still needed for all platforms.
 /* expat_config.h.cmake.  Based upon generated expat_config.h.in.  */
 
 /* 1234 = LIL_ENDIAN, 4321 = BIGENDIAN */
-#  define BYTEORDER 1234
+#define BYTEORDER 1234
 
 /* Define to 1 if you have the `arc4random' function. */
 /* #undef HAVE_ARC4RANDOM */
@@ -259,7 +259,7 @@ still needed for all platforms.
 /* #undef HAVE_DLFCN_H */
 
 /* Define to 1 if you have the <fcntl.h> header file. */
-#  define HAVE_FCNTL_H
+#define HAVE_FCNTL_H
 
 /* Define to 1 if you have the `getpagesize' function. */
 /* #undef HAVE_GETPAGESIZE */
@@ -268,64 +268,64 @@ still needed for all platforms.
 /* #undef HAVE_GETRANDOM */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#  define HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H
 
 /* Define to 1 if you have the `bsd' library (-lbsd). */
 /* #undef HAVE_LIBBSD */
 
 /* Define to 1 if you have the <memory.h> header file. */
-#  define HAVE_MEMORY_H
+#define HAVE_MEMORY_H
 
 /* Define to 1 if you have a working `mmap' system call. */
 /* #undef HAVE_MMAP */
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#  define HAVE_STDINT_H
+#define HAVE_STDINT_H
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#  define HAVE_STDLIB_H
+#define HAVE_STDLIB_H
 
 /* Define to 1 if you have the <strings.h> header file. */
 /* #undef HAVE_STRINGS_H */
 
 /* Define to 1 if you have the <string.h> header file. */
-#  define HAVE_STRING_H
+#define HAVE_STRING_H
 
 /* Define to 1 if you have `syscall' and `SYS_getrandom'. */
 /* #undef HAVE_SYSCALL_GETRANDOM */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#  define HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#  define HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H
 
 /* Define to 1 if you have the <unistd.h> header file. */
 /* #undef HAVE_UNISTD_H */
 
 /* Name of package */
-#  define PACKAGE "expat"
+#define PACKAGE "expat"
 
 /* Define to the address where bug reports for this package should be sent. */
-#  define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
+#define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
 
 /* Define to the full name of this package. */
-#  define PACKAGE_NAME "expat"
+#define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#  define PACKAGE_STRING "expat 2.4.1"
+#define PACKAGE_STRING "expat 2.4.4"
 
 /* Define to the one symbol short name of this package. */
-#  define PACKAGE_TARNAME "expat"
+#define PACKAGE_TARNAME "expat"
 
 /* Define to the home page for this package. */
-#  define PACKAGE_URL ""
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#  define PACKAGE_VERSION "2.4.1"
+#define PACKAGE_VERSION "2.4.4"
 
 /* Define to 1 if you have the ANSI C header files. */
-#  define STDC_HEADERS
+#define STDC_HEADERS
 
 /* whether byteorder is bigendian */
 /* #undef WORDS_BIGENDIAN */
@@ -336,23 +336,23 @@ still needed for all platforms.
 
 /* Define to specify how much context to retain around the current parse
    point. */
-#  define XML_CONTEXT_BYTES 1024
+#define XML_CONTEXT_BYTES 1024
 
-#  if ! defined(_WIN32)
+#if ! defined(_WIN32)
 /* Define to include code reading entropy from `/dev/urandom'. */
 /* #undef XML_DEV_URANDOM */
-#  endif
+#endif
 
 /* Define to make parameter entity parsing functionality available. */
-#  define XML_DTD
+#define XML_DTD
 
 /* Define to make XML Namespaces functionality available. */
-#  define XML_NS
+#define XML_NS
 
 /* Define to __FUNCTION__ or "" if `__func__' does not conform to ANSI C. */
-#  ifdef _MSC_VER
-#    define __func__ __FUNCTION__
-#  endif
+#ifdef _MSC_VER
+#  define __func__ __FUNCTION__
+#endif
 
 /* Define to `long' if <sys/types.h> does not define. */
 /* #undef off_t */
