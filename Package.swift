ff380f82836cfc8c7b27a29aa85b6bf44b556fba
Andrew Druk <adruk@readdle.com>
2023-03-22 23:19:54 +0200

    Dev: add Apple platforms support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
 // swift-tools-version:5.4
+import Foundation
 import PackageDescription
+
+let configHeaderSearchPath: String
+if ProcessInfo.processInfo.environment["BUILD_ANDROID"] != nil {
+    configHeaderSearchPath = "../../config/android"
+} else {
+    configHeaderSearchPath = "../../config/apple"
+}
 
 let package = Package(
     name: "expat",
@@ -12,7 +20,7 @@ let package = Package(
                 exclude: ["Makefile.am", "winconfig.h"],
                 publicHeadersPath: ".",
                 cSettings: [
-                    .headerSearchPath("../../config/android"),
+                    .headerSearchPath(configHeaderSearchPath),
                     .define("XML_POOR_ENTROPY")
                 ])
     ],
